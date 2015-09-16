3833858a15badfb3ddbb262649ed9c5c186dc1ba
Wu <alicewu1992@gmail.com>
2015-09-16 16:47:51 -0700

    setting up JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import authorizeGuests from "./authorize";
 
 let app = angular.module("team-tweet", []);
+let members = ['@codinghouse', '@agilelabs'];
 
-app.controller("TweetCtrl", () => {
+app.controller("TweetCtrl", ($scope) => {
+  $scope.mem = members;
 });
 
 authorizeGuests();
