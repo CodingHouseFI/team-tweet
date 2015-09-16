5e152db2b3481b17b855e8765c8e7e23f2f019c2
Jameela Huq <jameela.huq@gmail.com>
2015-09-16 15:57:19 -0700

    HTML mockup for tweet page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 let hot = "HOT";
 console.log(`ES2015 is ${hot}`);
+
+let authGuests = () => {
+  if (localStorage["firebase:session::twiffer"]) {
+    return;
+  }
+};
+
+var ref = new Firebase("https://twiffer.firebaseio.com");
+ref.authWithOAuthPopup("twitter", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+
+authGuests();
