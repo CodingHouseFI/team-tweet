b847c392fe02d032b77b236876202f5066864cae
Samer Buna <samer.buna@gmail.com>
2015-09-17 15:24:09 -0700

    Store and read the array in FB

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -1,12 +1,23 @@
-app.service("accountService", function() {
-  var accounts = [];
+app.service("accountService", function($timeout) {
+  var currentAdmin = JSON.parse(localStorage["firebase:session::teamtweet15"]).twitter.username;
+  var adminRef = new Firebase("https://teamtweet15.firebaseio.com/adminAccounts/" + currentAdmin);
+
+  var accountsForCurrentAdmin = [];
   this.getAll = function () {
-    return accounts;
+    return accountsForCurrentAdmin;
   };
 
-  this.add = function(username) {
-    if(accounts.indexOf(username) === -1){
-      accounts.push(username);
+  adminRef.on("child_added", function(snapshot) {
+    var twitterHandle = snapshot.val();
+    $timeout(function() {
+      accountsForCurrentAdmin.push(twitterHandle);
+    }, 0);
+  });
+
+  this.addAccount = function(twitterHandle) {
+    if(accountsForCurrentAdmin.indexOf(twitterHandle) === -1){
+      adminRef.push(twitterHandle);
+      // accountsForCurrentAdmin.push(twitterHandle);
     }
   };
 });
