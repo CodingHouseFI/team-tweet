aa56632729cfd46b6c7e8972fae30edfd3964857
Samer Buna <samer.buna@gmail.com>
2015-09-17 16:48:07 -0700

    Use object to store authorized accounts

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -3,7 +3,7 @@ app.service("accountService", function($timeout) {
   var iCanTweetAsRef, authorizedRef = new Firebase("https://teamtweet15.firebaseio.com/authorizedTweeters/" + loggedInAccount);
   var loggedInTweetAsRef = new Firebase("https://teamtweet15.firebaseio.com/ICanTweetAsAccounts/" + loggedInAccount);
 
-  var authorizedForLoggedInAccount = [], accountsYouCanTweetAs = [];
+  var authorizedForLoggedInAccount = {}, accountsYouCanTweetAs = [];
 
   this.getAll = function () {
     return authorizedForLoggedInAccount;
@@ -14,9 +14,10 @@ app.service("accountService", function($timeout) {
   };
 
   authorizedRef.on("child_added", function(snapshot) {
+    var fbKey = snapshot.key();
     var twitterHandle = snapshot.val();
     $timeout(function() {
-      authorizedForLoggedInAccount.push(twitterHandle);
+      authorizedForLoggedInAccount[fbKey] = twitterHandle;
     }, 0);
   });
 
@@ -29,10 +30,10 @@ app.service("accountService", function($timeout) {
 
   this.addAccount = function(twitterHandle) {
     twitterHandle = twitterHandle.toLowerCase();
-    if(authorizedForLoggedInAccount.indexOf(twitterHandle) === -1){
+    // if(authorizedForLoggedInAccount.indexOf(twitterHandle) === -1){
       authorizedRef.push(twitterHandle);
       iCanTweetAsRef = new Firebase("https://teamtweet15.firebaseio.com/ICanTweetAsAccounts/" + twitterHandle);
       iCanTweetAsRef.push(loggedInAccount);
-    }
+    // }
   };
 });
